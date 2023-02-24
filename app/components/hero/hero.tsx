32cd4ec08774379b989bde1a3cef52ed75255ebd
DH <daniel@vendettistudios.com>
2023-02-23 17:56:59 -0800

    hero background

--- app/components/hero/hero.tsx
+++ app/components/hero/hero.tsx
@@ -1,11 +1,12 @@
 'use client'
-
+import styles from './hero.module.css'
 type Props = {}
 
 const hero = (props: Props) => {
     return (
         <div className="videoHero">
-            <div className="backgroundVideo">
+            <div className={styles.overlay}></div>
+            <div className={styles.backgroundVideo}>
                 <iframe src="https://player.vimeo.com/video/777748066?background=1&loop=1" width="2800" height="1575"></iframe>
             </div>
         </div>
