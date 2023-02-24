f79f83645845c18ba82cb2a41c956df214619c2d
DH <daniel@vendettistudios.com>
2023-02-23 16:22:10 -0800

    setting up background-hero

---
+++ app/components/hero/hero.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+type Props = {}
+
+const hero = (props: Props) => {
+    return (
+        <div className="videoHero">
+            <div className="backgroundVideo">
+                <iframe src="https://player.vimeo.com/video/777748066?background=1&loop=1" width="2800" height="1575"></iframe>
+            </div>
+        </div>
+    )
+}
+
+export default hero
