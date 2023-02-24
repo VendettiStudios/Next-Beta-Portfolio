8d3a53a29bb1ae0875408db5a37dfe69b14a558b
DH <daniel@vendettistudios.com>
2023-02-24 06:19:13 -0800

    improving performance

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body style={{ backgroundImage: `url('/background.png')`, zIndex: -1 }}>{children}</body>
+      <body style={{ backgroundImage: `url(https://imagedelivery.net/WuNJ9qXloNU2BNpKhSqjvQ/3b24d98c-b979-431e-6c9b-46a650b2ff00/public)`, zIndex: -1 }}>{children}</body>
     </html>
   )
 }
